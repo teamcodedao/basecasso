36eb4a6bb790c92db1968c44dcc04ca24cf29caa
Nghiep <me@nghiep.dev>
2024-04-07 10:42:20 +0700

    Add socials

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,22 @@ export default function Home() {
           objectFit: 'cover',
         }}
       />
+      <div className='absolute left-14 top-14 flex gap-x-10 [&>a]:drop-shadow-2xl'>
+        <a href={process.env.NEXT_PUBLIC_TWITTER_URL} target='_twitter'>
+          <img
+            src='/twitter.svg'
+            alt='twitter'
+            className='h-16 sm:h-14 2xl:h-16 aspect-square'
+          />
+        </a>
+        <a href={process.env.NEXT_PUBLIC_TELEGRAM_URL} target='_telegram'>
+          <img
+            src='/telegram.svg'
+            alt='telegram'
+            className='h-16 sm:h-14 2xl:h-16 aspect-square'
+          />
+        </a>
+      </div>
     </main>
   );
 }
